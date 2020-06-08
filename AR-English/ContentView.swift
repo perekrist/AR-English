7b284e50db681b3c7d71eca33c1c29527a817dad
perekrist <perekrist31@gmail.com>
2020-06-08 18:39:56 +0700

    Initial Commit

---
+++ AR-English/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AR-English
+//
+//  Created by Кристина Перегудова on 08.06.2020.
+//  Copyright © 2020 Кристина Перегудова. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
