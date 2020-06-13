4e276fd59b19cc641ac54699dc982d6062fa8db0
perekrist <perekrist31@gmail.com>
2020-06-13 19:51:50 +0700

    created game filed
    * added models
    
    * added 3d words
    
    hide models when cards flip down

--- AR-English/Memo/ARMemoViewController.swift
+++ AR-English/Memo/ARMemoViewController.swift
@@ -13,6 +13,7 @@ import Combine
 class ARMemoViewController: UIViewController {
 
     @IBOutlet var arView: ARView!
+    var words: [String] = ["car", "plane", "solider", "robot", "car", "plane", "solider", "robot"]
         
     var anchor: AnchorEntity = AnchorEntity()
     
@@ -42,7 +43,57 @@ class ARMemoViewController: UIViewController {
             anchor.addChild(card)
         }
         
+        // hide 3d models when card flip down
+        let boxSize: Float = 0.7
+        let occlusionBoxMesh = MeshResource.generateBox(size: boxSize)
+        let occlusionBox = ModelEntity(mesh: occlusionBoxMesh, materials: [OcclusionMaterial()])
+        occlusionBox.position.y = -boxSize / 2 - 0.001
+        anchor.addChild(occlusionBox)
+        
+        var cancellable: AnyCancellable? = nil
+        cancellable = ModelEntity.loadModelAsync(named: words[0])
+            .append(ModelEntity.loadModelAsync(named: words[1]))
+            .append(ModelEntity.loadModelAsync(named: words[2]))
+            .append(ModelEntity.loadModelAsync(named: words[3]))
+            .append(ModelEntity.loadModelAsync(named: words[4]))
+            .append(ModelEntity.loadModelAsync(named: words[5]))
+            .append(ModelEntity.loadModelAsync(named: words[6]))
+            .append(ModelEntity.loadModelAsync(named: words[7]))
+            .collect()
+            .sink(receiveCompletion: { error in
+                print("Error: \(error)")
+                cancellable?.cancel()
+            }, receiveValue: { entities in
+                var objects: [ModelEntity] = []
+                for index in 0 ..< entities.count {
+                    // add 3d model
+                    let entity = entities[index]
+                    entity.setScale(SIMD3<Float>(0.002, 0.002, 0.002), relativeTo: self.anchor)
+                    entity.generateCollisionShapes(recursive: true)
+                    objects.append(entity.clone(recursive: true))
+                    
+                    // add 3d text
+                    let mesh1 = MeshResource.generateText(self.words[index])
+                    let color1 = UIColor.systemBlue
+                    let material1 = UnlitMaterial(color: color1)
+                    let entity1 = ModelEntity(mesh: mesh1, materials: [material1])
+                    entity1.setScale(SIMD3<Float>(0.0015, 0.0015, 0.0015), relativeTo: self.anchor)
+                    entity1.generateCollisionShapes(recursive: true)
+                    objects.append(entity1)
+                }
+                // shuffle cards
+                objects.shuffle()
+                        
+                for (index, objects) in objects.enumerated() {
+                    cards[index].addChild(objects)
+                    cards[index].transform.rotation = simd_quatf(angle: .pi, axis: [1, 0, 0])
+                }
+                
+                cancellable?.cancel()
+            })
     }
+        
+    
     
     @IBAction func onTap(_ sender: UITapGestureRecognizer) {
         let tapLocation = sender.location(in: arView)
