d836ae0e9368bbf636b5db5a414007f3efcb644e
Daria <50192991+daratayver@users.noreply.github.com>
2020-06-23 21:45:29 +0700

    for dictionary
    stupid dasha forgot this file (sorry)

---
+++ AR-English/Word.swift
@@ -0,0 +1,16 @@
+//
+//  Word.swift
+//  AR-English
+//
+//  Created by Дарья Перевертайло on 13.06.2020.
+//  Copyright © 2020 Дарья Перевертайло. All rights reserved.
+//
+
+import Foundation
+
+struct Word: Identifiable {
+    var id: Int
+    var name: String
+    var translate: String
+    var image: String
+}
