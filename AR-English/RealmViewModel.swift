9fcffb602f15f3b8bd94712f05a39fce91346fbe
Kristina <perekrist31@gmail.com>
2020-07-02 17:25:38 +0700

    created saving word func
    * added extra params to simple Word.swift
    
    * conert Word.swift -> DBWord.swift for saving

--- AR-English/RealmViewModel.swift
+++ AR-English/RealmViewModel.swift
@@ -12,4 +12,24 @@ import RealmSwift
 
 class RealmViewModel: ObservableObject {
     
+    @Published var words: [DBWord] = []
+    
+    let realm = try! Realm()
+    
+    func saveWord(word: Word) {
+        let newWord = DBWord()
+        newWord.name = word.name
+        newWord.translate = word.translate
+        newWord.image = word.image
+        newWord.priority = word.priority
+        newWord.descriptin = word.descriptin
+        newWord.use = word.use
+        
+        try! realm.write {
+            realm.add(newWord)
+        }
+    }
+    
+    
+    
 }
--- AR-English/View/Child Zone/DictionaryView.swift
+++ AR-English/View/Child Zone/DictionaryView.swift
@@ -10,18 +10,18 @@ import SwiftUI
 
 struct DictionaryView: View {
     
-    @State private var words: [Word] = [Word(id: 0, name: "Banana", translate: "Банан", image: "banana", priority: "new"),
-                                        Word(id: 1, name: "Coffee", translate: "Кофе", image: "coffee", priority: "new"),
-                                        Word(id: 2, name: "Green", translate: "Зеленый", image: "green", priority: "new"),
-                                        Word(id: 3, name: "Blue", translate: "Синий", image: "blue", priority: "done"),
-                                        Word(id: 4, name: "Cat", translate: "Кот", image: "cat", priority: "done"),
-                                        Word(id: 5, name: "Dog", translate: "Собака", image: "dog", priority: "done")
+    @State private var words: [Word] = [Word(id: 0, name: "Banana", translate: "Банан", image: "banana", priority: "new", descriptin: "", use: ""),
+                                        Word(id: 1, name: "Coffee", translate: "Кофе", image: "coffee", priority: "new", descriptin: "", use: ""),
+                                        Word(id: 2, name: "Green", translate: "Зеленый", image: "green", priority: "new", descriptin: "", use: ""),
+                                        Word(id: 3, name: "Blue", translate: "Синий", image: "blue", priority: "done", descriptin: "", use: ""),
+                                        Word(id: 4, name: "Cat", translate: "Кот", image: "cat", priority: "done", descriptin: "", use: ""),
+                                        Word(id: 5, name: "Dog", translate: "Собака", image: "dog", priority: "done", descriptin: "", use: "")
     ]
     
     @State private var showWordDescription = false
     @State private var query: String = ""
     
-    @State var word: Word = Word(id: 0, name: "Banana", translate: "Банан", image: "banana", priority: "new")
+    @State var word: Word = Word(id: 0, name: "Banana", translate: "Банан", image: "banana", priority: "new", descriptin: "", use: "")
     
     var body: some View {
         VStack {
